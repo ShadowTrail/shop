10139ed156b3572185c0128afc90aff4b92f1c72
ShadowTrail <anjaliburman12.com>
2023-01-27 11:11:42 +0530

    unfinished

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+// import Cart from "../Cart/Cart";
+// import { useSelector } from "react-redux";
+
+const Navbar = () => {
+  // const [open, setOpen] = useState(false);
+  // const products = useSelector((state) => state.cart.products);
+
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/7/70/Flag_India_37x37.jpg"
+              alt=""
+            />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>INR</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/1">
+              Women
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/2">
+              Men
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/3">
+              Kids
+            </Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link className="link" to="/">
+            LA MODE
+          </Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link className="link" to="/">
+              Homepage
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              About
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Contact
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Stores
+            </Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div
+              className="cartIcon"
+              // onClick={() => setOpen(!open)}
+            >
+              <ShoppingCartOutlinedIcon />
+              {/* <span>{products.length}</span> */}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* {open && <Cart />} */}
+    </div>
+  );
+};
+
+export default Navbar;
